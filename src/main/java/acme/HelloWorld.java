132451609d5a895168177117f877afc22640eb0a
KarthikeyanLearning <Karthiksoft23@gmail.com>
2021-03-05 21:46:02 -0800

    Added 1st Java File

---
+++ src/main/java/acme/HelloWorld.java
@@ -0,0 +1,11 @@
+package acme;
+
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("This is First Line");
+        System.out.println("This is First Line");
+        System.out.println("This is First Line");
+        System.out.println("This is First Line");
+    }
+}
